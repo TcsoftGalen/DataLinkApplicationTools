9b6907759d118ee2ced2e31be28b98fd48412573
ZongJie Chen <ZongJie Chen>
2017-05-03 02:29:47 +0800

    添加项目文件。

---
+++ DataLinkApplicationTools/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows;
+using System.Net.NetworkInformation;
+using System.Management;
+
+namespace DataLinkApplicationTools
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            foreach (NetworkInterface adapter in adapters)
+            {
+                //以太网
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    IPInterfaceProperties ip = adapter.GetIPProperties();
+                    UnicastIPAddressInformationCollection ipCollection = ip.UnicastAddresses;
+                    foreach (UnicastIPAddressInformation ipadd in ipCollection)
+                    {
+                        if (ipadd.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork && adapter.OperationalStatus == OperationalStatus.Up)
+                        {
+
+                            switch (adapter.Name)
+                            {
+                                case "以太网":
+                                    LB.Items.Add("名称：" + adapter.Name);
+                                    LB.Items.Add("地址：" + ipadd.Address.ToString());
+                                    LB.Items.Add("状态：" + "已连接");
+                                    break;
+                                case "本地连接1":
+                                    LB_2.Items.Add("名称：" + adapter.Name);
+                                    LB_2.Items.Add("地址：" + ipadd.Address.ToString());
+                                    LB_2.Items.Add("状态：" + "已连接");
+                                    break;
+
+
+                            }
+
+                        }
+                        else
+                        {
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    LB_2.Items.Add(MethodClass.GetSystemVersion());
+                    LB_2.Items.Add(MethodClass.GetIisServerVersion());
+                    LB_2.Items.Add(MethodClass.GetSqlServerVersion(1));
+                    LB_2.Items.Add("名称：" + adapter.Name);                 
+                    LB_2.Items.Add("状态：" + "已连接");
+                }
+                
+            }
+        }
+    }
+}
---
+++ DataLinkApplicationTools/SqlOperation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataLinkApplicationTools
+{
+     class SqlOperation
+    {
+
+        /// <summary>
+        /// 建立数据库连接.
+        /// </summary>
+        /// <param name="sql_ConnectionString">SQL库连接字串</param>
+        /// <returns>返回SqlConnection对象</returns>
+        public SqlConnection GetSqlServerConnection(string sql_ConnectionString)
+        {
+            SqlConnection mySqlConnection = new SqlConnection(sql_ConnectionString);
+            return mySqlConnection;
+        }
+        ///<summary>
+        ///执行SqlCommand
+        ///<summary>
+        ///<param name="sql_ExecuteSQL"</param>
+        public void ExecuteSQLAction(string sql_ExecuteSQL)
+        {
+            SqlConnection conn = this.GetSqlServerConnection("123");
+            conn.Open();
+            try
+            {
+                SqlCommand sqlCmd = new SqlCommand(sql_ExecuteSQL,conn);
+                int a  = int.Parse(sqlCmd.ExecuteNonQuery().ToString());
+                sqlCmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                conn.Close();
+                           
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
