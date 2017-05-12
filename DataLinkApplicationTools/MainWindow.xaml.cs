8d89173b59126056f340a34cfae1ff0211fd7f08
ZongJie Chen <galen.chen@tcsoft.net.cn>
2017-05-13 05:43:52 +0800

    增加注释。

--- DataLinkApplicationTools/MainWindow.xaml.cs
+++ DataLinkApplicationTools/MainWindow.xaml.cs
@@ -26,7 +26,11 @@ namespace DataLinkApplicationTools
         }
 
        
-    
+        /// <summary>
+        /// 一键安装按钮被按下 触发一键安装事件。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AutoInstallButtomClick(object sender, RoutedEventArgs e)
         {
             SynchronizationContext contexrt = SynchronizationContext.Current;
@@ -40,6 +44,11 @@ namespace DataLinkApplicationTools
            
 
         }
+        /// <summary>
+        /// 线程执行方法，此方法调用PublicTools中的SwitchDataBase以在线程中还原数据库。
+        /// 并使用context.send 更新UI
+        /// </summary>
+        /// <param name="sx"></param>
         private void  ResDB(object sx)
         {
             SynchronizationContext uicon = sx as SynchronizationContext;
@@ -53,7 +62,10 @@ namespace DataLinkApplicationTools
             uicon.Send(FlashUI, pt.sql_Result);
             uicon.Send(FlashUI, "wooooooooooow!");
         }
-
+        /// <summary>
+        /// 线程回调方法。
+        /// </summary>
+        /// <param name="str">线程执行的结果</param>
        private void FlashUI(object str)
         {
             string text = str as string;
--- DataLinkApplicationTools/PublicTools.cs
+++ DataLinkApplicationTools/PublicTools.cs
@@ -10,14 +10,16 @@ namespace DataLinkApplicationTools
 {
     public class PublicTools
     {
-        //IIS 当前版本号
-       // private string iis_VersionString;
-        //OS 当前内部版本号 6.1 08  6.3 12
-        private string os_CurrentVersion;
+
         //OS产品名称
         private string os_ProductName;
+        //OS 当前内部版本号 6.1 08  6.3 12
+        private string os_CurrentVersion;
+       
         //计算机网卡信息
         private string net_Info;
+
+        //测试用
         public string sql_Result;
         public string sql_Result1;
 
@@ -25,6 +27,7 @@ namespace DataLinkApplicationTools
         protected static string SqlConnectionString;
         /// <summary>
         /// 类构造函数,在构造时初始化 ConfigurationManager
+        /// 并取得数据库连接字符。
         /// 无返回值;
         /// </summary>
         public PublicTools()
@@ -33,20 +36,24 @@ namespace DataLinkApplicationTools
             SqlConnectionString = appSetting.AppSettings.Settings["connectString"].Value.ToString();         
         }
         /// <summary>
-        /// 取得系统信息   是 Svr08 或 Svr12
+        /// 取得操作系统信息，并在Main中显示。
+        /// 用于未来对Server 2012 的支持。
+        /// 目前阶段没有对版本进行处理。
         /// </summary>
         public string GetSystemInfo()
         {   //regedit系统信息路径
             RegistryKey regKey = Registry.LocalMachine.OpenSubKey(Properties.Resources.REG_OSInfoPath);
             //取得系统名称
             os_ProductName = regKey.GetValue("ProductName").ToString();
+            //取得内部版本号  08 6.1   12 6.3
             os_CurrentVersion = regKey.GetValue("CurrentVersion").ToString();         
             regKey.Close();
-
             return "当前操作系统：" + os_ProductName + " " + os_CurrentVersion;
         }
 
-
+        /// <summary>
+        /// 线程测试
+        /// </summary>
         public void ABC()
         { int i;
             for ( i = 0; i < 500; i++)
@@ -57,6 +64,9 @@ namespace DataLinkApplicationTools
             
 
         }
+        /// <summary>
+        /// 线程测试。
+        /// </summary>
         public void DEF()
         {
             int j;
@@ -67,7 +77,10 @@ namespace DataLinkApplicationTools
             sql_Result1 = "2号数据库还原完毕";
 
         }
-
+        /// <summary>
+        /// 取得计算机网络连接信息。
+        /// 待整理。
+        /// </summary>
         private void GetNetWorkInfo()
         {
             NetworkInterface[] ins = NetworkInterface.GetAllNetworkInterfaces();
@@ -90,11 +103,12 @@ namespace DataLinkApplicationTools
             }
         }
         /// <summary>
-        /// 取得SQL连接。
+        /// 取得SqlConnection对象。
         /// </summary>
-        /// <returns></returns>
+        /// <returns>返回已个SqlConnection对象，该连接未Open，在引用是需要手动Open</returns>
         private SqlConnection GetSqlConnection()
         {
+            //创建连接对象
             SqlConnection sqlConnection;
             if (null != SqlConnectionString)
             {
@@ -107,7 +121,12 @@ namespace DataLinkApplicationTools
             return sqlConnection;
         }
 
-
+        /// <summary>
+        /// 根据数据库代码来还原数据库。
+        /// 此方法将在Main中的Thread调用。
+        /// 每次调用该方法时将对sql_Result的值进行清空，以防在线程执行完毕后反馈UI信息有误。
+        /// </summary>
+        /// <param name="DataBase">待还原的数据库代码 1=DL，2=CDB ，3=Log ，4=QC</param>
         public void SwitchDataBase(int DataBase)
         {
             sql_Result = "";
@@ -127,12 +146,17 @@ namespace DataLinkApplicationTools
                     break;
             }
         }
-       private  void RestoreDataLinkDB(string DbName,string RestoreString)
+        /// <summary>
+        /// 执行还原数据库
+        /// </summary>
+        /// <param name="DataBaseName">待还原的数据库名称</param>
+        /// <param name="RestoreString">还原数据的脚本语句，此语句已固话在Resoures的引用中</param>
+       private  void RestoreDataLinkDB(string DataBaseName,string RestoreString)
         {
-            if (File.Exists(@"C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\"+ DbName + ".mdf"))
+            if (File.Exists(@"C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\"+ DataBaseName + ".mdf"))
             {
                 //数据库存在
-               sql_Result = "数据库 [ "+ DbName + " ] 已存在！"; ;
+               sql_Result = "数据库 [ "+ DataBaseName + " ] 已存在！"; ;
             }
             else
             {
@@ -146,7 +170,11 @@ namespace DataLinkApplicationTools
                         Connection = sqlcon,
                         CommandText = RestoreString
                     };
-                    sql_Result = sqlcmd.ExecuteNonQuery().ToString();
+                    if (sqlcmd.ExecuteNonQuery() == -1)
+                    {
+                        sql_Result = "数据库 [ " + DataBaseName + " ] 还原完毕！";
+                    }
+
                     sqlcmd.Dispose();
                 }
                 catch (SqlException ex)
@@ -158,10 +186,15 @@ namespace DataLinkApplicationTools
                     sqlcon.Close();
                     sqlcon.Dispose();
                 }
-                sql_Result = "数据库 [ "+ DbName + " ] 还原完毕！"; 
+                
             }
 
         }    
+        /// <summary>
+        /// 创建DataLink程序所对应的文件夹。
+        /// 并移动相关文件到正确的位置。
+        /// 施工ing
+        /// </summary>
         public void CreateDir()
         {
             if (!Directory.Exists(@"D:\DataLink")) { Directory.CreateDirectory(@"D:\DataLink"); }
